620c7516623f9a9f1cbc416d94b82c71074c1485
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-09-17 18:05:11 +0300

    1.12 done?

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,20 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
 
-const App = () => {
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
 
-  const hello = "hello"
+let Randomizer = random => {
+  let randomNum = Math.floor(Math.random() * 6)
+  return randomNum
+}
+
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+
+  const setValue = newValue => {
+    setSelected(newValue)
+  }
+
+  const Random = Randomizer
 
   return (
     <div>
-      {hello}
+      {props.anecdotes[selected]}
+      <br></br>
+      {console.log(Random)}
+      <Button handleClick={() => setValue(Random)} text="next anecdote"/>
     </div>
+    
   )
 }
 
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root'))
