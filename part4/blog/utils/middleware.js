c82a0315b6d3b5cb90a9857c6f0d2c2d0ded1ced
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-11-04 19:13:47 +0200

    4.20 done

--- part4/blog/utils/middleware.js
+++ part4/blog/utils/middleware.js
@@ -19,13 +19,28 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
     return response.status(400).json({ error: error.message })
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({ error: 'invalid token or token missing' })
   }
 
   next(error)
 }
 
+const tokenExtractor = (request, response, next) => {
+  const token = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+      return authorization.substring(7)
+    }
+    return null
+  }
+  request.token = token(request)
+  next()
+}
+
 module.exports = {
   requestLogger,
   unknownEndpoint,
-  errorHandler
+  errorHandler,
+  tokenExtractor
 }
