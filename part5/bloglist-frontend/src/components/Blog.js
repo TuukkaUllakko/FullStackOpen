57f114c1991d7514641c1f7253841ecbb1700666
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-11-21 17:55:10 +0200

    5.7 done

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,38 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+
+  const [showAll, setShowAll] = useState(false)
+
+  const hideShowAll = { display: showAll ? 'none' : '' }
+  const showShowAll = { display: showAll ? '' : 'none' }
+
+  const like = () => {
+    console.log('make this function for liking blogs later')
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div style={hideShowAll}>
+        {blog.title} by {blog.author} <button onClick={() => setShowAll(true)}>View</button>
+      </div>
+      <div style={showShowAll}>
+        {blog.title} by {blog.author} <button onClick={() => setShowAll(false)}>Hide</button>
+        <div>{blog.url}</div>
+        <div>Likes: {blog.likes} <button onClick={like}>Like!</button></div>
+      </div>
+    </div>
+  )
+
+}
+
 
 export default Blog
