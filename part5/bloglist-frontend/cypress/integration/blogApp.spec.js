21dc20753cccb8d25a2084a9256d1e3e62d35b41
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-11-25 17:41:05 +0200

    5.17 and 5.18 done

--- part5/bloglist-frontend/cypress/integration/blogApp.spec.js
+++ part5/bloglist-frontend/cypress/integration/blogApp.spec.js
@@ -1,10 +1,39 @@
 describe('Blog app', function () {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
+
+    const user = {
+      name: 'Matti Luukkainen',
+      username: 'mluukkai',
+      password: 'salainen'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users', user)
+
     cy.visit('http://localhost:3000')
   })
 
   it('Login form is shown', function () {
     cy.contains('Login to view blogs')
   })
+
+  describe('Login', function () {
+
+    it('succeeds with correct credentials', function () {
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('salainen')
+      cy.get('#login-button').click()
+
+      cy.contains('Matti Luukkainen logged in')
+      cy.contains('logout')
+    })
+
+    it('fails with wrong credentials', function () {
+      cy.get('#username').type('mluukkai')
+      cy.get('#password').type('wrong')
+      cy.get('#login-button').click()
+
+      cy.get('.error').contains('wrong username or password')
+    })
+  })
+
 })
