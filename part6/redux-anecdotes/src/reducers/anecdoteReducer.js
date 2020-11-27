a516fac9d836d154f7e1c9dd23a186c967cde4df
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-11-27 20:27:40 +0200

    6.3 done

--- part6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -23,7 +23,19 @@ const reducer = (state = initialState, action) => {
   console.log('state now: ', state)
   console.log('action', action)
 
-  return state
+  switch (action.type) {
+    case 'VOTE': {
+      const id = action.data.id
+      const anecdoteToBeUpdated = state.find(anec => anec.id === id)
+      const updatedAnecdote = { ...anecdoteToBeUpdated }
+
+      return state.map(anecdote =>
+        anecdote.id !== id ? anecdote : updatedAnecdote)
+    }
+    default:
+      return state
+  }
+
 }
 
 export default reducer
