a37c0ad90088fb4713acc1cb45be267db416dc58
TuukkaUllakko <tuukka.ullakko@centria.fi>
2020-11-28 17:27:10 +0200

    fix 6.9

--- part6/redux-anecdotes/src/store.js
+++ part6/redux-anecdotes/src/store.js
@@ -2,6 +2,6 @@ import reducer from './reducers/anecdoteReducer'
 import { createStore } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 
-const store = createStore(reducer, composeWithDevTools)
+const store = createStore(reducer, composeWithDevTools())
 
 export default store
